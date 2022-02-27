9c5e4ee9f6206bc1bf98abe95065c793a8a05c7e
Dave Webb <dave-riroriro@dcw.co.nz>
2022-02-27 22:29:17 +1300

    More notes about the plan

--- main.go
+++ main.go
@@ -8,15 +8,30 @@ A cookie/js-free proxy for imgur links, inspired by https://imgin.voidnet.tech/
 
 Just replace the imgur hostname with the hostname of improxy in your browser.
 
-Basic plan is the same as https://imgin.voidnet.tech/
-
 - Support basic imgur paths like:
-  - /gallery/YUJYQ
-  - /YUJYQ
-  - /a/YUJYQ
-- Work by fetching and parsing ingur HTML, then caching images and other media locally.
+	- /gallery/YUJYQ
+	- /a/YUJYQ
+	- /YUJYQ.jpg
+- Work by fetching and parsing ingur HTML to get download links for the media.
+- Then download the media, temporarily cache it, and serve simple HTML responses embedding the media.
 - Require no cookies or javascript to function.
 
+Steps:
+	1. Parse imgur path from request.
+	2. Fetch the HTML from imgur.
+	3. Parse the HTML to get media download links.
+	4. Download media and cache locally.
+	5. Return a HTML response doc that embeds the local media.
+	6. Serve local media requests.
+
+Notes:
+- The imgur HTML currently holds a JSON document which seems to enumerate all
+the media, and all their titles/descriptions and other metadata. Parsing this
+JSON looks the best first step towards getting a list of media to download.
+- The proxy needs a way of mapping the ordered list of media and metadata with
+the cached files, so the generated HTML response shows things in order and uses
+the correct descriptions etc. Perhaps store a subset of the imgur JSON in the
+cache as well, with some standard imgur path to filename mapping.
 */
 
 func main() {
