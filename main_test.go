c7d24ad8fcae29e6d6e7098fe3ed8bda140ea3eb
Dave Webb <dave-riroriro@dcw.co.nz>
2022-03-04 23:05:32 +1300

    Tests now actually test

--- main_test.go
+++ main_test.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestImgurGet(t *testing.T) {
 
-	path := "/49jzlTB"
-	got := ImgurGet(path)
+	{
+		path := "/49jzlTB"
+		got := ImgurGet(path)
+		expected := 1
+		// fmt.Printf("%v\n", got)
+		if len(got.Media) != expected {
+			t.Errorf("Expected len=%v but got len=%v", expected, len(got.Media))
+		}
 
-	fmt.Println(got)
-
-	path = "/gallery/kaOZU"
-	got = ImgurGet(path)
-
-	fmt.Println(got)
+	}
+	{
+		path := "/gallery/kaOZU"
+		got := ImgurGet(path)
+		expected := 10
+		// fmt.Printf("%v\n", got)
+		if len(got.Media) != expected {
+			t.Errorf("Expected len=%v but got len=%v", expected, len(got.Media))
+		}
+	}
 }
